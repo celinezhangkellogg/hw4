fe9e31feea4a5ab56fc7df34c2eeb160c198f57c
Xinyi Zhang <celine.zhang@kellogg.northwestern.edu>
2024-06-05 13:09:03 +0000

    prepared for production

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -9,20 +9,16 @@ class EntriesController < ApplicationController
   end
 
   def create
-    @current_user = User.find_by({ "id" => session["user_id"] })
-  if @current_user != nil
     @entry = Entry.new
     @entry["title"] = params["title"]
     @entry["description"] = params["description"]
     @entry["occurred_on"] = params["occurred_on"]
     @entry["place_id"] = params["place_id"]
-    @entry.uploaded_image.attach(params["image"])
+    @entry.uploaded_image.attach(params["uploaded_image"])
     # when create an entry, it is assigned to whoever logged in
     @entry["user_id"] = session["user_id"]
     @entry.save
-  else 
-    flash["notice"] = "Login first."
-  end
+ 
     redirect_to "/places/#{@entry["place_id"]}"
   end
 
